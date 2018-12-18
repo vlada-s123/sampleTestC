8a52aeff88f595f481ff574857bda58a0eef6d3f
obilyk <oleg.bilyk@onseo.biz>
2018-12-18 16:32:35 +0200

    init

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,30 @@
+/**
+ * Created by kate on 1/12/16.
+ */
+
+public class Calculator {
+
+    public static void main(String[] args) {
+    }
+
+    public int add(int a, int b) {
+
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+
+        return a * b;
+    }
+
+    //TODO
+    //add methods for multiply, division, subtraction, square root, x^2
+
+}
+
+
+
+
+
+
+
---
+++ src/test/java/AddTwoValuesTests.java
@@ -0,0 +1,42 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by kate on 1/12/16.
+ */
+
+
+public class AddTwoValuesTests {
+
+    Calculator calc = new Calculator();
+
+    @Before
+    public void consolePrint() {
+        System.out.println("Tests started!!! ");
+    }
+
+    @Test
+    public void testAddTwoPositiveValues() {
+        assertEquals("10 + 5 must be 15", 15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testAddTwoNegativeValues() {
+        assertEquals("-10 + -5 must be -15", -15, calc.add(-10, -5));
+    }
+
+    //TODO
+    //add new tests for add() method
+
+    //TODO
+    //add tests for new methods
+
+    @After
+    public void consolePrintAfter() {
+        System.out.println("Tests finished!!! ");
+
+    }
+}
